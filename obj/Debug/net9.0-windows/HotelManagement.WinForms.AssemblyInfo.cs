b8d9a7822c83bdd511ae0b9e22a084588b22c324
Abdurahman <abdurahman.i.work@gmail.com>
2026-04-21 23:31:55 +0200

    nnn

--- obj/Debug/net9.0-windows/HotelManagement.WinForms.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/HotelManagement.WinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement.WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ad70e4bbf194816a283d43af5b0a6b0d46e20c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a456fe23294f011ebe77e1dde93e569ce5bc0c12")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement.WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement.WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
